7e4044479837da80b484e382171b5150fb545d06
Galib4 <54716504+Galib4@users.noreply.github.com>
2020-04-19 12:36:53 +0530

    Add pie-chart component data

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SingleDataSet, Label } from 'ng2-charts';
+import { ChartType, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-pie-chart',
@@ -7,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieChartComponent implements OnInit {
 
-  constructor() { }
+  // Pie
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = [['ABC'], ['DEF'], 'GHI'];
+  public pieChartData: SingleDataSet = [300, 500, 100];
+  public pieChartType: ChartType = 'pie';
+  public colors: any [] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#111'
+    }
+  ];
+
+  constructor() {}
+
 
   ngOnInit(): void {
   }
