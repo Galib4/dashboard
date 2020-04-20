227bb15e33cd8be60b1331193768f5d49d1b7e8a
Galib4 <54716504+Galib4@users.noreply.github.com>
2020-04-20 19:11:14 +0530

    Created server component

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Server } from '../../shared/server';
+
+const SAMPLE_SERVERS = [
+  {id: 1, name: 'Dev 1', isOnline: true},
+  {id: 2, name: 'Dev 2', isOnline: false},
+  {id: 3, name: 'Dev 3', isOnline: true},
+  {id: 4, name: 'Dev 4', isOnline: true}
+];
 
 @Component({
   selector: 'app-section-health',
@@ -8,6 +16,8 @@ import { Component, OnInit } from '@angular/core';
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
+
+  servers: Server [] = SAMPLE_SERVERS;
 
   ngOnInit(): void {
   }
--- src/app/sections/section-sales/section-sales.component.ts
+++ src/app/sections/section-sales/section-sales.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Server } from '../../shared/server';
+
+
+const SAMPLE_SERVERS = [
+  {id: 1, name: 'Sales', isOnline: true},
+  {id: 2, name: 'Web', isOnline: false},
+  {id: 3, name: 'Tele', isOnline: true},
+  {id: 4, name: 'Customer', isOnline: true}
+];
 
 @Component({
   selector: 'app-section-sales',
@@ -8,6 +17,8 @@ import { Component, OnInit } from '@angular/core';
 export class SectionSalesComponent implements OnInit {
 
   constructor() { }
+
+  servers: Server [] = SAMPLE_SERVERS;
 
   ngOnInit(): void {
   }
